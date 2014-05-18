1cc878b0c6032d07ee2817b3ed9962f1760a267f
Luke Gary <lukegary@gmail.com>
2014-05-18 02:05:00 -0400

    no message

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,11 +10,11 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main',
+        templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
       .when('/about', {
-        templateUrl: 'views/about',
+        templateUrl: 'views/about.html',
         controller: 'MainCtrl'
       })
       .otherwise({
